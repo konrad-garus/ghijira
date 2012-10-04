70af6110ffc9e59625accb7f3cc76319a75b06fa
konrad-garus <konrad.garus@gmail.com>
2012-10-04 07:56:08 +0200

    Purely functional implementation, passing bare parameters all the way

--- src/ghijira/core.clj
+++ src/ghijira/core.clj
@@ -19,15 +19,6 @@
 
 (def UNNAMED "UNNAMED")
 
-(def ^:dynamic *config-id*)
-(def ^:dynamic *ghuser*)
-(def ^:dynamic *ghproject*)
-(def ^:dynamic *auth*)
-(def ^:dynamic *maxcmt*)
-(def ^:dynamic *user-map*)
-(def ^:dynamic *jira-project*)
-(def ^:dynamic *git-base-url*)
-
 ; Use the all-pages mechanism in tentacles to retrieve a complete list
 ; of issues. I found that several of the GH export scripts on the web
 ; ignore everything past the first page, making them useless for all
@@ -38,21 +29,22 @@
 
 ;; Loading issues from GH
 
-(defn get-issues [state]
-  (issues/issues *ghuser* *ghproject* {:auth *auth* :all-pages true :state state}))
+(defn get-issues [ghuser ghproject auth state]
+  (issues/issues ghuser ghproject {:auth auth :all-pages true :state state}))
 
-(defn get-all-issues [] 
-  (concat (get-issues "open")
-          (get-issues "closed")))
+(defn get-all-issues [ghuser ghproject auth] 
+  (concat (get-issues ghuser ghproject auth "open")
+          (get-issues ghuser ghproject auth "closed")))
 
-(defn assoc-comments-and-events [issue]
+(defn assoc-comments-and-events [ghuser ghproject auth issue]
   (println (str "Fetching data for #" (:number issue)))
   (assoc issue 
-         :comment-contents (issues/issue-comments *ghuser* *ghproject* (:number issue) {:auth *auth*})
-         :event-contents (issues/issue-events *ghuser* *ghproject* (:number issue) {:auth *auth*})))
+         :comment-contents (issues/issue-comments ghuser ghproject (:number issue) {:auth auth})
+         :event-contents (issues/issue-events ghuser ghproject (:number issue) {:auth auth})))
 
-(defn issues-with-extra []
-  (map assoc-comments-and-events (get-all-issues)))
+(defn issues-with-extra [ghuser ghproject auth]
+  (let [issues (get-all-issues ghuser ghproject auth)]
+    (map (partial assoc-comments-and-events ghuser ghproject auth) issues)))
 
 ; Cache for 30 minutes, for easier development at the REPL
 (def issues-with-extra-cached (memoize/memo-ttl issues-with-extra (* 30 60 1000)))
@@ -76,7 +68,7 @@
 
 ;; Export to JIRA
 
-(defn columns [] 
+(defn columns [maxcmt] 
   (concat
     ["Issue Id",
      "Summary",
@@ -89,7 +81,7 @@
      "Resolution",
      "Reporter",
      "Labels"]
-    (repeat *maxcmt* "Comments") ))
+    (repeat maxcmt "Comments") ))
 
 ; Date-time format used by the Github Issues API
 (def gh-formatter (tf/formatters :date-time-no-ms))
@@ -102,10 +94,10 @@
 (defn gh2jira [date]
   (tf/unparse jira-formatter (tf/parse gh-formatter date)))
 
-(defn get-user [issue]
+(defn get-user [user-map issue]
   (let [u (or (:login (:user issue))
               (:login (:actor issue)))]
-    (get *user-map* u u)))
+    (get user-map u u)))
 
 (defn cross-item-ref-replace
   ""
@@ -115,20 +107,20 @@
     (str/replace \# \_ ))) ; Drop #, JIRA does not like.
 
 (defn comment-or-event-to-text
-  [c]
+  [jira-project git-base-url c]
   (cond
     (= (:event c) "referenced") (str "Referenced in commit:\n"
-                                     *git-base-url*
+                                     git-base-url
                                      (:commit_id c)
                                      "\n")
     (:event c) (str (:event c))
-    :else  (cross-item-ref-replace (:body c) *jira-project*)))
+    :else  (cross-item-ref-replace (:body c) jira-project)))
 
-(defn format-comment [c]
+(defn format-comment [user-map jira-project git-base-url c]
   (let [created-at (tf/parse gh-formatter (:created_at c))
-        comment-text (comment-or-event-to-text c)]
+        comment-text (comment-or-event-to-text jira-project git-base-url c)]
     (str "Comment:"
-         (get-user c)
+         (get-user user-map c)
          ":"
          (tf/unparse jira-formatter created-at)
          ":" \newline \newline
@@ -145,11 +137,11 @@
 ;(map (juxt :created_at (comp :login :actor) :event :commit_id) (:event-contents x))
 
 
-(defn issue2row [issue]
+(defn issue2row [maxcmt user-map jira-project git-base-url issue]
   (let [filtered-events (remove #(= "subscribed" (:event %)) (:event-contents issue))
         all-comments (concat (:comment-contents issue)
                              filtered-events)
-        trimmed-comments (take *maxcmt* 
+        trimmed-comments (take maxcmt 
                                (sort-by :created_at all-comments))
         milestone (:title (:milestone issue))
         milestone-dashes (str/replace (or milestone "") \space \-)]
@@ -161,23 +153,26 @@
         (gh2jira (:created_at issue))
         (gh2jira (:updated_at issue))
         "Task" ; issue type
+        (get-milestone issue)
         milestone-dashes
         (if (= "closed" (:state issue)) "Closed" "Open")
         (if (= "closed" (:state issue)) "Fixed" "Unresolved")
-        (get-user issue)
+        (get-user user-map issue)
         (get-labels issue))
-      (map format-comment trimmed-comments)
-      (repeat (- *maxcmt* (count trimmed-comments)) "")    ; pad out field count  
+      (map (partial format-comment user-map jira-project git-base-url) trimmed-comments)
+      (repeat (- maxcmt (count trimmed-comments)) "")    ; pad out field count  
     )))
 
-(defn export-issues-to-file [issues filename]
+(defn format-issues-to-export [maxcmt user-map jira-project git-base-url issues]
   (let [issues-in-order (sort-by :number issues)]
+    (concat
+      [(columns maxcmt)] 
+      (map 
+        (partial issue2row maxcmt user-map jira-project git-base-url) issues-in-order))))
+
+(defn export-to-file [export filename]
     (with-open [out-file (io/writer filename)]
-      (csv/write-csv 
-        out-file
-        (concat
-          [(columns)] 
-          (map issue2row issues-in-order))))))
+      (csv/write-csv out-file export )))
 
 ;; Main
 
@@ -186,30 +181,28 @@
   (println "project_id should correspond to a config file, e.g. 'my-project' for 'config-my-project.clj'")) 
 
 (defn load-config [config-id]
-  (with-open [r (io/reader (str "config-" config-id ".clj"))]
-    (read (java.io.PushbackReader. r))))
+  (let [filename (str "config-" config-id ".clj")]
+    (read-string (slurp filename))))
 
 (defn process
   "main program"
   [config-id]
-    (let [config (load-config config-id)]
-    (binding [
-            *config-id* config-id
-            *ghuser* (:ghuser config)
-            *ghproject* (:ghproject config)
-            *auth* (:auth config)
-            *maxcmt* (:maxcmt config)
-            *user-map* (:user-map config)
-            *jira-project* (:jira-project config)
-            *git-base-url* (:git-base-url config)
-            ]
-    (let [issues (issues-with-extra-cached)]
+  (let [config (load-config config-id)
+        ghuser (:ghuser config)
+        ghproject (:ghproject config)
+        auth (:auth config)
+        maxcmt (:maxcmt config)
+        user-map (:user-map config)
+        jira-project (:jira-project config)
+        git-base-url (:git-base-url config)
+        ]
+    (let [issues (issues-with-extra-cached ghuser ghproject auth)
+          export (format-issues-to-export maxcmt user-map jira-project git-base-url issues)]
       (warn-missing-issues issues)
-      (export-issues-to-file issues (str "JIRA-" *config-id* ".csv"))))))
+      (export-to-file export (str "JIRA-" config-id ".csv")))))
 
 (defn -main [& args]
   (when (empty? args)
     (print-usage)
     (System/exit 1))
   (process (first args)))
-
